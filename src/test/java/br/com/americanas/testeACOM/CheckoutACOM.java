dfafe662eed2a6f40b8373f7719238fe8c63ec71
BIRDRED <gustt103@uol.com.br>
2015-05-08 10:48:38 -0300

    Teste de Checkout

--- src/test/java/br/com/americanas/testeACOM/CheckoutACOM.java
+++ src/test/java/br/com/americanas/testeACOM/CheckoutACOM.java
@@ -3,12 +3,11 @@ package br.com.americanas.testeACOM;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
-import junit.framework.TestCase;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -16,7 +15,7 @@ import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class CopyOfLoginACOM  {
+public class CheckoutACOM  {
 
 	public WebDriver driver;
 	private String baseUrl;
@@ -29,16 +28,29 @@ public class CopyOfLoginACOM  {
 		ffox.setEnvironmentProperty("DISPLAY", ":0");
 		driver = new FirefoxDriver(ffox, fp);
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		baseUrl = "https://carrinho.americanas.com.br/CustomerWeb/pages/Login/";
+		baseUrl = "http://www.americanas.com.br/produto/120736101/game-need-for-speed-rivals-ps4";
 	}
 	
 	@Test
 	public void LoginFirefox() throws InterruptedException{
     	driver.get(baseUrl);
     	Wait<WebDriver> wait = new WebDriverWait(driver, 15);
-    	driver.findElement(By.cssSelector("#LoginForm .userData , .email")).sendKeys("test_fitanalytics@b2wdigital.com");
-    	driver.findElement(By.cssSelector("#LoginForm .userData , .pasw")).sendKeys("testWAFA");
-    	driver.findElement(By.id("btProsseguir")).click();
+    	driver.manage().deleteAllCookies();
+        Thread.sleep(7000);
+        Cookie gtmsac = new Cookie("gtm_sac", "ucla");
+    	driver.manage().addCookie(gtmsac);
+    	driver.findElement(By.cssSelector(".spt-b-main-buy")).click();
+        Thread.sleep(5000);
+    	driver.findElement(By.cssSelector(".list-actions #upper-buy-button")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.id("account-email")).sendKeys("test_fitanalytics@b2wdigital.com");
+    	driver.findElement(By.id("account-password")).sendKeys("testWAFA");
+    	driver.findElement(By.id("button-login-customer")).click();
+    	Thread.sleep(5000);
+    	driver.findElement(By.cssSelector(".boleto")).click();
+    	Thread.sleep(5000);
+    	driver.findElement(By.cssSelector(".buttons .withStep .next")).click();
+    	Thread.sleep(5000);
         driver.close();
 	}
 	
